812870401ab14623d8c85d89520b9644576a4ab7
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-09-01 17:48:25 +0700

    Initial Commit

---
+++ Ideal-ist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ideal-ist
+//
+//  Created by Erlangga Anugrah Arifin on 01/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
